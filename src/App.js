76d7d9edb265829d6a29fb5e8e27c8432ce2cbfa
Tiffany <pekyuan@gmail.com>
2023-05-15 23:14:13 +0800

    fixed bug and update readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import Axios from "axios";
 import { Formik, Field, Form } from 'formik';
 import List from '@mui/material/List'
@@ -9,105 +9,115 @@ import ListItemText from '@mui/material/ListItemText';
 
 function App() {
 
-    //initial coordinate values
-    const initialBegCoorValues = {
-        lat_beg:'', 
-        long_beg: '', 
-    };
-    const initialEndCoorValues = {
-        lat_end: '', 
-        long_end: '',
-    };
-    //initial value of the input form
-    const initialPlace = {
-        begin:'',
-        end:''
-    };
-    //set begin and end input (the place to go, for example CC22)
-    //TODO: can be further optimized 
-    const [begin, setBegin] = useState("");
-    const [end, setEnd] = useState("");
-    //begin and end coordinate
-    const [begCor, setBegCor] = useState(initialBegCoorValues);
-    const [endCor, setEndCor] = useState(initialEndCoorValues);
-    //route result
-    const [route, setRoute] = useState([]);
+  //initial coordinate values
+  const initialBegCoorValues = {
+    lat_beg:'', 
+    long_beg: '', 
+  };
+  const initialEndCoorValues = {
+    lat_end: '', 
+    long_end: '',
+  };
+  //initial value of the input form
+  const initialPlace = {
+    begin:'',
+    end:''
+  };
+  //set begin and end input (the place to go, for example CC22)
+  //TODO: can be further optimized 
+  const [begin, setBegin] = useState("");
+  const [end, setEnd] = useState("");
+  //prevent getBeginCoor and getEndCoor from being called when first mount
+  const shouldRender = useRef(false);
+  //begin and end coordinate
+  const [begCor, setBegCor] = useState(initialBegCoorValues);
+  const [endCor, setEndCor] = useState(initialEndCoorValues);
+  //route result
+  const [route, setRoute] = useState([]);
 
-    //when submit the form, update the value of begin and end 
-    const handleFormSubmit = (values) => {
-        setBegin(values.begin);
-        setEnd(values.end);
-    };
+  //when submit the form, update the value of begin and end 
+  const handleFormSubmit = (values) => {
+    //getBeginCoor and end equiv should only be called when there is value
+    shouldRender.current = true;
+    setBegin(values.begin);
+    setEnd(values.end);
+  };
 
-    //api call to get begin and end coordinate
-    const getBeginCoor = () => {
-        Axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${begin}&returnGeom=Y&getAddrDetails=N&pageNum=1`)
-            .then((res) => {
-                //updte the coordinate after the api call
-                setBegCor({
-                    lat_beg: res.data.results[0].LATITUDE,
-                    long_beg: res.data.results[0].LONGITUDE
-                });
-            });
-        console.log("get begin"); //debug purpose
-    };
+  //api call to get begin and end coordinate
+  const getBeginCoor = () => {
+    if (shouldRender.current == false) {
+      return;
+    }
+    Axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${begin}&returnGeom=Y&getAddrDetails=N&pageNum=1`)
+      .then((res) => {
+        //updte the coordinate after the api call
+        setBegCor({
+          lat_beg: res.data.results[0].LATITUDE,
+          long_beg: res.data.results[0].LONGITUDE
+        });
+      });
+    console.log("get begin"); //debug purpose
+  };
 
-    const getEndCoor = () => {
-        Axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${end}&returnGeom=Y&getAddrDetails=N&pageNum=1`)
-            .then((res) => {
-                //updte the coordinate after the api call
-                setEndCor({
-                    lat_end: res.data.results[0].LATITUDE,
-                    long_end: res.data.results[0].LONGITUDE
-                });
-            });
-        console.log("get end"); //debug purpose
-    };
+  const getEndCoor = () => {
+    if (shouldRender.current == false) {
+      //updte the coordinate after the api call
+      return;
+    }
+    Axios.get(`https://developers.onemap.sg/commonapi/search?searchVal=${end}&returnGeom=Y&getAddrDetails=N&pageNum=1`)
+      .then((res) => {
+        //updte the coordinate after the api call
+        setEndCor({
+          lat_end: res.data.results[0].LATITUDE,
+          long_end: res.data.results[0].LONGITUDE
+        });
+      });
+    console.log("get end"); //debug purpose
+  };
 
-    //fetch trip information 
-    const fetchTrip = () => {
-        Axios.get(`https://developers.onemap.sg/privateapi/routingsvc/route?start=${begCor.lat_beg}%2C${begCor.long_beg}&end=${endCor.lat_end}%2C${endCor.long_end}%2C&routeType=pt&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEwMjkwLCJ1c2VyX2lkIjoxMDI5MCwiZW1haWwiOiJwZWt5dWFuQGdtYWlsLmNvbSIsImZvcmV2ZXIiOmZhbHNlLCJpc3MiOiJodHRwOlwvXC9vbTIuZGZlLm9uZW1hcC5zZ1wvYXBpXC92MlwvdXNlclwvc2Vzc2lvbiIsImlhdCI6MTY4MzczMjEzMiwiZXhwIjoxNjg0MTY0MTMyLCJuYmYiOjE2ODM3MzIxMzIsImp0aSI6ImVkNzZiNDdlZGU0OTJmN2UxZjQ2ZTZjOGJiNTY4YmFiIn0.W_EZXGzCM_UP1bMi5RRLFMmY0kbEicOwrJyxtbKwIKw&date=2023-03-12&time=15%3A30%3A00&mode=RAIL&maxWalkDistance=1000`)
-            .then((res) => {
-                var result = [];
-                var subway_result = res.data.plan.itineraries[0].legs.filter(x => x.mode == "SUBWAY");
-                subway_result.forEach(x => {result.push(x.from.name + " (" + x.from.stopCode + ")");
-                    result.push(x.intermediateStops.map(y => y.name + " (" + y.stopCode + ")"));
-                    result.push(x.to.name + " (" + x.to.stopCode + ")");
-                }
-                );
-                console.log(result.flat()); //debug purpose
-                setRoute(result.flat());
-            });
-    }; 
+  //fetch trip information 
+  const fetchTrip = () => {
+    Axios.get(`https://developers.onemap.sg/privateapi/routingsvc/route?start=${begCor.lat_beg}%2C${begCor.long_beg}&end=${endCor.lat_end}%2C${endCor.long_end}%2C&routeType=pt&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEwMjkwLCJ1c2VyX2lkIjoxMDI5MCwiZW1haWwiOiJwZWt5dWFuQGdtYWlsLmNvbSIsImZvcmV2ZXIiOmZhbHNlLCJpc3MiOiJodHRwOlwvXC9vbTIuZGZlLm9uZW1hcC5zZ1wvYXBpXC92MlwvdXNlclwvc2Vzc2lvbiIsImlhdCI6MTY4MzczMjEzMiwiZXhwIjoxNjg0MTY0MTMyLCJuYmYiOjE2ODM3MzIxMzIsImp0aSI6ImVkNzZiNDdlZGU0OTJmN2UxZjQ2ZTZjOGJiNTY4YmFiIn0.W_EZXGzCM_UP1bMi5RRLFMmY0kbEicOwrJyxtbKwIKw&date=2023-03-12&time=15%3A30%3A00&mode=RAIL&maxWalkDistance=1000`)
+      .then((res) => {
+        var result = [];
+        var subway_result = res.data.plan.itineraries[0].legs.filter(x => x.mode == "SUBWAY");
+        subway_result.forEach(x => {result.push(x.from.name + " (" + x.from.stopCode + ")");
+          result.push(x.intermediateStops.map(y => y.name + " (" + y.stopCode + ")"));
+          result.push(x.to.name + " (" + x.to.stopCode + ")");
+        }
+        );
+        console.log(result.flat()); //debug purpose
+        setRoute(result.flat());
+      });
+  }; 
 
-    //begin and end coordinate will be get after the value of begin and end change, ie after form submittion
-    useEffect(getBeginCoor, [begin]);
-    useEffect(getEndCoor, [end]);
+  //begin and end coordinate will be get after the value of begin and end change, ie after form submittion
+  useEffect(getBeginCoor, [begin]);
+  useEffect(getEndCoor, [end]);
 
-    return (
-        <div className="App">
-        <Formik 
+  return (
+    <div className="App">
+      <Formik 
         initialValues= {initialPlace}
         onSubmit = {handleFormSubmit}> 
         <Form>
-        <Field id="begin" name="begin" placeholder="CC22"/>
-        <Field id="end" name="end" placeholder="CC25"/>
-        <button type="submit"> Update </button>
+          <Field id="begin" name="begin" placeholder="CC22"/>
+          <Field id="end" name="end" placeholder="CC25"/>
+          <button type="submit"> Update </button>
         </Form>
-        </Formik>
-        <button onClick={fetchTrip}> Show result </button>
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      </Formik>
+      <button onClick={fetchTrip}> Show result </button>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
         {route.map((value) => (
-            <ListItem
+          <ListItem
             key={value}
-            disableGutters
-            >
+            disableGutters>
             <ListItemText primary={`${value}`} />
-            </ListItem>
+          </ListItem>
         ))}
-        </List>
-        </div>
-    );
+      </List>
+    </div>
+  );
 }
 
 export default App;
